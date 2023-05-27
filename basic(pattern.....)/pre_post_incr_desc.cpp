bb1b89514b6c9299508ba8bcb9d9b157c503d542
AVVKavvk <kumawatvipin066@gmail.com>
2023-05-27 15:09:08 +0530

    c

---
+++ basic(pattern.....)/pre_post_incr_desc.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define pi 3.14
+#define fl(a, b) for (int i = a; i < b; i++)
+#define ma(a, b) ((a) > (b) ? (a) : (b));
+#define mi(a, b) ((a) < (b) ? (a) : (b));
+#define pb push_back
+#define ps(a, b) std::cout << a << ' ' << b << std::endl;
+#define pn(a) std::cout << a << std::endl;
+#define p(a) std::cout << a;
+#define vet vector<char>
+using namespace std;
+#define ve vector<int>
+#define pu(n) push_back(n);
+
+int main()
+{
+    int a=5;
+    // cin >> a;
+    cout << (++a) * (++a) << endl;
+    return 0;
+}
