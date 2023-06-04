70a18103b39786a3bc659e0bdbd7ac9bb3ddda3e
AVVKavvk <kumawatvipin066@gmail.com>
2023-06-04 09:53:54 +0530

    9.0

---
+++ pointers/1.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+#define pi 3.14
+#define fli(a,b) for(int i=a;i<b;i++)
+#define flj(a,b) for(int j=a;j<b;j++)
+#define flk(a,b) for(int k=a;k<b;k++)
+#define ma(a,b)      ( (a) > (b) ? (a) : (b)); 
+#define mi(a,b)      ( (a) < (b) ? (a) : (b)) ;
+#define pb push_back 
+#define pob pop_back 
+#define sz size() 
+#define p2s(a,b) std::cout <<a<<' ' << b << std::endl ;
+#define pn(a) std::cout <<a << std::endl ;
+#define nl std::cout << std::endl ;
+#define p(a) std::cout <<a ;
+#define ps(a) std::cout <<a<< ' ' ;
+#define cig(arr,n)   cin.getline(arr,n) 
+#define sza(a) sizeof(a)/sizeof(int)
+#define vec vector<char> 
+#define v2e vector <vector<int> > 
+#define v2ec vector <vector<char> >
+#define vei vector<int> 
+#define pu(n) push_back( n); 
+ 
+
+int main() {
+int a=5;
+char b='a';
+double d=.002;
+
+int * ptr1=&a;
+char * ptr2=&b;
+double * ptr3=&d;
+pn(sizeof(ptr1));
+pn(sizeof(ptr2));
+pn(sizeof(ptr3));
+
+
+//*           Null Pointer            
+int * ptr=nullptr;
+
+
+
+
+return 0 ;
+}
---
+++ pointers/diffBtwArrayAndPointer.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+#define pi 3.14
+#define fli(a,b) for(int i=a;i<b;i++)
+#define flj(a,b) for(int j=a;j<b;j++)
+#define flk(a,b) for(int k=a;k<b;k++)
+#define ma(a,b)      ( (a) > (b) ? (a) : (b)); 
+#define mi(a,b)      ( (a) < (b) ? (a) : (b)) ;
+#define pb push_back 
+#define pob pop_back 
+#define sz size() 
+#define p2s(a,b) std::cout <<a<<' ' << b << std::endl ;
+#define pn(a) std::cout <<a << std::endl ;
+#define nl std::cout << std::endl ;
+#define p(a) std::cout <<a ;
+#define ps(a) std::cout <<a<< ' ' ;
+#define cig(arr,n)   cin.getline(arr,n) 
+#define sza(a) sizeof(a)/sizeof(int)
+#define vec vector<char> 
+#define v2e vector <vector<int> > 
+#define v2ec vector <vector<char> >
+#define vei vector<int> 
+#define pu(n) push_back( n); 
+ 
+
+int main() {
+
+    //?                   1   
+
+int arr[10];
+pn(sizeof(arr));
+int *ptr=arr;
+pn(sizeof(ptr));
+
+//?                       2
+ 
+arr=arr+1;  //    *    error        //?   l value is constant
+
+ptr=ptr+1 ;  //*         no error          
+
+return 0 ;
+}
