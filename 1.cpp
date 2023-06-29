29444d2c3e03b6728b95af18994496a15e79d89a
AVVKavvk <kumawatvipin066@gmail.com>
2023-06-29 21:55:36 +0530

    32.1

--- 1.cpp
+++ 1.cpp
@@ -1,36 +1,82 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define pi 3.14
-#define fli(a, b) for (int i = a; i < b; i++)
-#define flj(a, b) for (int j = a; j < b; j++)
-#define flk(a, b) for (int k = a; k < b; k++)
-#define ma(a, b) ((a) > (b) ? (a) : (b));
-#define mi(a, b) ((a) < (b) ? (a) : (b));
-#define pb push_back
-#define pob pop_back
-#define sz size()
-#define p2s(a, b) std::cout << a << ' ' << b << std::endl;
-#define pn(a) std::cout << a << std::endl;
-#define nl std::cout << std::endl;
-#define p(a) std::cout << a;
-#define ps(a) std::cout << a << ' ';
-#define cig(arr, n) cin.getline(arr, n)
-#define sza(a) sizeof(a) / sizeof(int)
-#define vec vector<char>
-#define v2e vector<vector<int>>
-#define v2ec vector<vector<char>>
-#define vei vector<int>
-#define pu(n) push_back(n);
-       
-int main()
-{
-  // int *a;
-  // cout<<sizeof(a);
+// #include <iostream>
 
-  char a[] = "vipin";
-  cout << a;
-  char *p = &a[0];
-  nl;
-  cout << p;
-  return 0;
-}                                       
+// class DynamicArray
+// {
+// public:
+//   int *data;
+//   int size;
+
+// public:
+//   DynamicArray(int size)
+//   {
+//     this->size = size;
+//     data = new int[size];
+//     std::cout << "DynamicArray constructor called" << std::endl;
+//   }
+//   // DynamicArray(int size) : size(size) {
+//   //     data = new int[size];
+//   //     std::cout << "DynamicArray constructor called" << std::endl;
+//   // }
+
+//   ~DynamicArray()
+//   {
+//     delete[] data;
+//     std::cout << "DynamicArray destructor called" << std::endl;
+//   }
+
+//   void setValue(int index, int value)
+//   {
+//     if (index >= 0 && index < size)
+//     {
+//       data[index] = value;
+//     }
+//   }
+
+//   int getValue(int index)
+//   {
+//     if (index >= 0 && index < size)
+//     {
+//       return data[index];
+//     }
+//     return -1; // Invalid index
+//   }
+// };
+
+// int main()
+// {
+//   DynamicArray arr(5);
+
+//   for (int i = 0; i < 5; i++)
+//   {
+//     arr.setValue(i, i + 1);
+//   }
+
+//   for (int i = 0; i < 5; i++)
+//   {
+//     std::cout << "Value at index " << i << ": " << arr.getValue(i) << std::endl;
+//   }
+//   delete arr.data;
+
+//   return 0;
+// }
+
+#include <iostream>
+
+class MyClass {
+public:
+    MyClass() {
+        std::cout << "Constructor called" << std::endl;
+    }
+
+    ~MyClass() {
+        std::cout << "Destructor called" << std::endl;
+    }
+};
+
+int main() {
+    MyClass obj; // Creating an object of MyClass
+
+    // Rest of the program
+
+    return 0;
+}
