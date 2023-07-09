144c924dd2f7eab754d6a4173e5e858280861db5
AVVKavvk <kumawatvipin066@gmail.com>
2023-07-09 10:30:24 +0530

    42

---
+++ leetcode/1962.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        priority_queue<int> maxHeap;
+
+        for(int i=0;i<piles.size();i++){
+            maxHeap.push(piles[i]);
+        }
+
+        while(k--){
+            int topElement=maxHeap.top();
+            maxHeap.pop();
+            topElement=topElement-floor(topElement/2);
+            maxHeap.push(topElement);
+        }
+
+        int sum=0;
+        while(!maxHeap.empty()){
+            int temp=maxHeap.top();
+            maxHeap.pop();
+            sum+=temp;
+        }
+        return sum;
+    }
+};
