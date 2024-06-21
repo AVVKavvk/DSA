fdbf203e42c654ab992bb970fdc81315178f5f7c
AVVKavvk <kumawatvipin066@gmail.com>
2024-06-21 08:46:04 +0530

    Trie class renamed to TrieNode, corresponding changes in function parameters and variable declarations in Trie/creation.cpp
    Changes in linkedList/singlyLL/deletion.cpp: class Node renamed to ListNode, corresponding changes in function parameters and variable declarations, and addition of a destructor in ListNode class.

--- Disjoint_Set/implementaion.cpp
+++ Disjoint_Set/implementaion.cpp
@@ -37,16 +37,16 @@ public:
         }
     }
 
-    int findUpar(int node)
+    int findParent(int node)
     {
         if (node == parent[node])
             return node;
-        return parent[node] = findUpar(parent[node]);
+        return parent[node] = findParent(parent[node]);
     }
 
     void unionBySize(int u, int v) {
-        int ulp_u = findUpar(u);
-        int ulp_v = findUpar(v);
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
         if (ulp_u == ulp_v) return;
         if (size[ulp_u] < size[ulp_v]) {
             parent[ulp_u] = ulp_v;
@@ -59,8 +59,8 @@ public:
     }
 
     void unionByRank(int u, int v) {
-        int ulp_u = findUpar(u);
-        int ulp_v = findUpar(v);
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
         if (ulp_u == ulp_v) return;
         if (rank[ulp_u] < rank[ulp_v]) {
             parent[ulp_u] = ulp_v;
@@ -85,7 +85,7 @@ int main()
     ds.unionBySize(6, 7);
     ds.unionBySize(5, 6);
 
-    if (ds.findUpar(3) == ds.findUpar(7))
+    if (ds.findParent(3) == ds.findParent(7))
     {
         cout << "same\n";
     }
@@ -94,7 +94,7 @@ int main()
         cout << "different\n";
     }
     ds.unionBySize(3, 7);
-    if (ds.findUpar(3) == ds.findUpar(7))
+    if (ds.findParent(3) == ds.findParent(7))
     {
         cout << "same\n";
     }
